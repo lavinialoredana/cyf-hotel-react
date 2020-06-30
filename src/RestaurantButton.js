a2ede0d801e1b315dce11883ddc11ea610a0533b
Lavinia Loredana <lavinialoredana@Lavinias-MacBook-Pro.local>
2020-07-01 00:59:14 +0200

    finish ex from Class 2

---
+++ src/RestaurantButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RestaurantButton = props => (
+  <button onClick={props.handleClick} className="btn btn-primary">
+    ADD
+  </button>
+);
+
+export default RestaurantButton;
